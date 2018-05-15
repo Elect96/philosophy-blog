1db369d11c48f5f47f4a589d31e54db0f409c351
Jakub Lamczyk <lamczykjakub@gmail.com>
2018-05-15 10:07:00 +0200

    components files init; shared sass files init

---
+++ src/app/components/main/main.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: `./main.component.html`,
+  styleUrls: ['./main.component.scss']
+})
+export class MainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
